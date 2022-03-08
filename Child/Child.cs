9ae509e004d19e607c7451519df3a69e390eb06f
Bouke Haarsma <bouke@haarsma.eu>
2022-03-08 10:55:30 +0100

    Use Assembly.LoadFile to simulate dynamic plugin

---
+++ Child/Child.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Child
+{
+    public class Child
+    {
+        public static void Play()
+        {
+            var ad = AppDomain.CurrentDomain;
+            Console.WriteLine("\r\nApplication domain '{0}': IsFullyTrusted = {1}",
+                ad.FriendlyName, ad.IsFullyTrusted);
+
+            Console.WriteLine("   IsFullyTrusted = {0} for the current assembly {1}",
+                typeof(Child).Assembly.IsFullyTrusted,
+                typeof(Child).Assembly);
+
+            Console.WriteLine("   IsFullyTrusted = {0} for mscorlib",
+                typeof(int).Assembly.IsFullyTrusted);
+
+            throw new Exception("Some exception");
+        }
+    }
+}
---
+++ Parent/Worker.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Security.Permissions;
+using System.Security.Policy;
+
+namespace Parent
+{
+    public class Worker : MarshalByRefObject
+    {
+        private static string childpath = Path.Combine(Path.GetDirectoryName(typeof(Worker).Assembly.Location), "Child.dll");
+
+        private static void Main()
+        {
+            var w = new Worker();
+            w.TestExceptionStacktrace();
+
+            var adSandbox = GetInternetSandbox();
+            var handle = Activator.CreateInstanceFrom(
+                adSandbox,
+                typeof(Worker).Assembly.ManifestModule.FullyQualifiedName,
+                typeof(Worker).FullName);
+            w = (Worker)handle.Unwrap();
+            w.TestExceptionStacktrace();
+        }
+
+        public void TestExceptionStacktrace()
+        {
+            TestInner();
+        }
+
+        private void TestInner()
+        {
+            var ass = Assembly.LoadFile(childpath);
+
+            var playMethod = ass.GetTypes()[0].GetMethod("Play");
+
+            try
+            {
+                playMethod.Invoke(null, Array.Empty<object>());
+            }
+            catch (Exception e)
+            {
+                var s = e.ToString();
+                Console.WriteLine("Stack trace contains {0}line numbers for Child assembly:",
+                    s.Split(new[] { Environment.NewLine }, StringSplitOptions.None)
+                        .Single(x => x.Contains("Play()")).Contains("line")
+                        ? ""
+                        : "no ");
+                Console.WriteLine($"   {s}");
+            }
+        }
+
+        // ------------ Helper method ---------------------------------------
+        private static AppDomain GetInternetSandbox()
+        {
+            // Create the permission set to grant to all assemblies.
+            var hostEvidence = new Evidence();
+            hostEvidence.AddHostEvidence(new Zone(
+                System.Security.SecurityZone.Internet));
+            var pset =
+                System.Security.SecurityManager.GetStandardSandbox(hostEvidence);
+
+            // add this to the permission set
+            pset.AddPermission(new FileIOPermission(
+                FileIOPermissionAccess.PathDiscovery, typeof(Worker).Assembly.Location)
+            );
+            pset.AddPermission(new FileIOPermission(
+                FileIOPermissionAccess.PathDiscovery | FileIOPermissionAccess.Read, Path.GetDirectoryName(childpath))
+            );
+
+            // Identify the folder to use for the sandbox.
+            var ads = new AppDomainSetup();
+            ads.ApplicationBase = System.IO.Directory.GetCurrentDirectory();
+
+            var fullTrustAssemblies = new[]
+            {
+                typeof(Worker).Assembly.Evidence.GetHostEvidence<StrongName>(),
+            };
+
+            // Create the sandboxed application domain.
+            return AppDomain.CreateDomain("Sandbox", hostEvidence, ads, pset, fullTrustAssemblies);
+        }
+    }
+}
